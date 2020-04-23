de0c68a10f9727f41035ad0a26ad35d4b450d890
Pratik Mishra <mishrapratik977@gmail.com>
2020-04-23 09:54:00 +0530

    Script that works as a calculator

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def sum(num1, num2):
+	sum = num1 + num2
+	# prints the sum of the number
+	print("The sum is:", sum)
+def diff(num1 - num2):
+	if num1 > num2:
+		diff = num1 -num2
+	else:
+		diff = num2 - num1
+	print("The difference is:", diff)
+
+def main():
+	sum(5,4)
+	diff(56, 65)
+	diff(8,12)
+main()
